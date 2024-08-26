71158e6bd7880bed10a37fc62ce68f8b30ef90eb
Zheng Yi <lwzhengyi@gmail.com>
2024-08-26 17:00:16 +0800

    deleted:    README.md 	new file:   chatbot/.env 	modified:   chatbot/package-lock.json 	modified:   chatbot/package.json 	deleted:    chatbot/src/App.css 	modified:   chatbot/src/App.js 	deleted:    chatbot/src/App.test.js 	modified:   chatbot/src/index.css 	modified:   chatbot/src/index.js 	deleted:    chatbot/src/logo.svg 	deleted:    chatbot/src/reportWebVitals.js 	new file:   chatbot/src/server.js 	deleted:    chatbot/src/setupTests.js

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
 
-function App() {
+
+const App = () => {
+  const [error, setError] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="app">
+      <section className="search-section">
+        <p>What do you want to know?
+          <button className="surprise">Surprise me</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className="input-container">
+            <input
+              value={""}
+              placeholder = "what do you want to know?"
+              onChange={""}
+
+            />
+            {!error && <button>Ask me</button>}
+            {error && <button>Clear</button>}
+          </div>
+
+          {error && <p>{error}</p>}
+          <div className="search-result">
+            <div>
+              <div key={""}>
+                <p className="answer"></p>
+              </div>
+            </div>
+          </div>
+
+      </section>
     </div>
   );
 }
